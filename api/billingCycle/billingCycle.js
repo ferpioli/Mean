4e67e6608ce57320d41a269af32d878f23136510
ferpioli <ferpiolli@gmail.com>
2017-10-28 00:36:22 -0200

    Realizando validacoes no backend

--- api/billingCycle/billingCycle.js
+++ api/billingCycle/billingCycle.js
@@ -6,18 +6,18 @@ const creditSchema = new mongoose.Schema({
   value: { type : Number, min:0, required: true}
 })
 
-const debtSchema= new mongoose.Schema({
+const debtSchema = new mongoose.Schema({
   name: { type: String , required :true},
-  value: {type: String ,min:0,required :true},
-  status:{type: String,required: false,uppercase: true,
+  value: {type: Number ,min:0, required: [true, 'Informe o valor do debito'] },
+  status:{type: String, required: false, uppercase: true,
   enum: ['PAGO','PENDENTE','AGENDADO']}
 
 })
 
 const billingCycleSchema = new mongoose.Schema({
   name:{type: String, required :true},
-  month:{type: Number, min:1,max:12,required:true},
-  year: {type: Number,min:1970,max:2100, required: true},
+  month:{type: Number, min:1, max:12, required:true},
+  year: {type: Number, min:1970, max:2100, required: true},
   credits: [creditSchema],
   debts:[debtSchema]
 
