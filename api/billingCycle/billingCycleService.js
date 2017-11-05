0eeca9098c5c5214dff5d42b0438956947f8f57c
ferpioli <ferpiolli@gmail.com>
2017-11-05 02:21:17 -0200

    uniformizando as mensagens de erro

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -1,7 +1,27 @@
+const _ = require('lodash')
 const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get','post','put','delete'])
 BillingCycle.updateOptions({new: true, runValidators: true })
+BillingCycle.after('post',sendErrosOrNext).after('put',sendErrosOrNext)
+
+function sendErrosOrNext(req, res, next) {
+  const bundle = res.locals.bundle
+  if(bundle.errors) {
+    var errors = parseErros(bundle.errors)
+    res.status(500).json({errors})
+
+  }else{
+    next()
+  }
+}
+
+function parseErros(nodeRestfulErrors){
+  const erros = []
+  _.forIn(nodeRestfulErrors, error => erros.push(error.message))
+  return errors
+}
+
 
 BillingCycle.route('count',function(req, res, next) {
   BillingCycle.count(function(error, value) {
